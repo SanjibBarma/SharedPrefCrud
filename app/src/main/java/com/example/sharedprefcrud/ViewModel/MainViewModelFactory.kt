a923def84a3f99450f666e0f0d1a619a31d07559
Admin <sanjib2barma@gmail.com>
2025-01-19 00:39:12 +0600

    shared pref

--- app/src/main/java/com/example/sharedprefcrud/ViewModel/MainViewModelFactory.kt
+++ app/src/main/java/com/example/sharedprefcrud/ViewModel/MainViewModelFactory.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 
 class MainViewModelFactory (private val context: Context): ViewModelProvider.Factory{
+    //create is used for create an instance of viewmodel
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         return MainViewModel(context) as T
     }
--- app/src/main/java/com/example/sharedprefcrud/Views/MainScreen.kt
+++ app/src/main/java/com/example/sharedprefcrud/Views/MainScreen.kt
@@ -7,7 +7,10 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -32,7 +35,9 @@ fun MainScreen (mainViewModel: MainViewModel){
     Column(
         modifier = Modifier
         .fillMaxSize()
-        .padding(16.dp),
+        .padding(16.dp)
+            .verticalScroll(rememberScrollState())
+            .imePadding(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ){
